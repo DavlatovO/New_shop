7c242431b318746129ed44b1dfc27d8be487d02a
DavlatovO <davlatovoybek007@gmail.com>
2024-04-30 18:38:33 +0500

    Changes have been made

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-def index(request):
-   return render(request, 'index.html')
+def index(request,):
+   products = models.Products.objects.all()
+   category = models.Category.objects.all()
 
+   context = {
+      'products':products,
+      'category':category
+   }
+   return render(request, 'index.html', context)
 
+def shop(request):
+
+   return render(request, 'shop.html')
