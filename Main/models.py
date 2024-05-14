a1a8032e918d7328b29f3e673c0408621ed3ecd7
DavlatovO <davlatovoybek007@gmail.com>
2024-05-14 21:32:17 +0500

    Changes have been made

--- Main/models.py
+++ Main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -24,6 +24,8 @@ class Products(models.Model):
     image = models.ImageField(upload_to='media/')
     quantity = models.IntegerField(default=0)
     slug= models.SlugField(blank=True, null=True)
+    product_discount = models.DecimalField(decimal_places=2, max_digits=10, 
+                                         blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:  # Generate slug only if it's not already set
@@ -36,7 +38,24 @@ class Products(models.Model):
 class Review(models.Model):
     mark = models.IntegerField()
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     
+    def __str__(self):
+        return f'{self.product.name}, {self.user.username}, {self.text}'
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            obj = Review.objects.filter(product=self.product, user=self.user).exclude(pk=self.pk).first()
+        else:
+            obj = Review.objects.filter(product=self.product, user=self.user).first()
+        
+        if obj:
+            obj.mark = self.mark
+            obj.text = self.text
+            obj.save() 
+        else:
+            super().save(*args, **kwargs)
+
 
 
--- Main/views.py
+++ Main/views.py
@@ -87,3 +87,7 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+
+
+
